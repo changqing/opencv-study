c55be3b038e26fc3abfd3e8ae6a8f7c251584333
Changqing Ye <changqing.ye8@gmail.com>
2016-10-11 15:45:48 +0800

    add a svm image classification example

---
+++ openCvSVM/CMakeFiles/MultipleimageWindow.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MultipleimageWindow.dir/main.cpp.o"
+  "CMakeFiles/MultipleimageWindow.dir/utils/MultipleImageWindow.cpp.o"
+  "MultipleimageWindow.pdb"
+  "MultipleimageWindow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MultipleimageWindow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ openCvSVM/CMakeFiles/OpenCVSVM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCVSVM.dir/main.cpp.o"
+  "CMakeFiles/OpenCVSVM.dir/utils/MultipleImageWindow.cpp.o"
+  "OpenCVSVM.pdb"
+  "OpenCVSVM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCVSVM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
